612de68f62466f3a7982423680afbeb7eda4831e
danishbilalansari <danishbilalansari@gmail.com>
2024-03-14 00:25:05 +0500

    Changes for Basket.API and consuming Discount.grpc service in Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 using Microsoft.OpenApi.Models;
 
 namespace Basket.API
@@ -28,6 +30,10 @@ namespace Basket.API
             });
 
             builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+            builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
+                o => { o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]); });
+
+            builder.Services.AddScoped<DiscountGrpcService>();
 
             var app = builder.Build();
 
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Discount.Grpc.Mapper;
 using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 
 internal class Program
 {
@@ -9,6 +10,8 @@ internal class Program
 
         builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
+        builder.Services.AddControllers();
+
         // Registering AutoMapper object with DiscountProfile class
         builder.Services.AddAutoMapper(typeof(DiscountProfile));
 
@@ -17,9 +20,22 @@ internal class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
 
-        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+        app.UseRouting();
+        _ = app.UseEndpoints(endpoints =>
+        {
+            _ = endpoints.MapGrpcService<DiscountService>();
+            _ = endpoints.MapControllers();
+            _ = endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+            });
+
+        });
 
         // Following will be used for migration database. It will drop and create the coupon table if exist and insert pre-defined values
         // Commenting following as it will be called each time when Discount.API project is executed
